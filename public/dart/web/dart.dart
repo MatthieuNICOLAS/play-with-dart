bf3fb2f164b5d9e55532e6d62c6dab43877b4c2c
Matthieu NICOLAS <matthieu.nicolas@telecomnancy.net>
2014-11-26 13:24:58 +0100

    Add AngularDart to the project

--- public/dart/web/dart.dart
+++ public/dart/web/dart.dart
@@ -1,16 +1,7 @@
 import 'dart:html';
+import 'package:angular/angular.dart';
+import 'package:angular/application_factory.dart';
 
 void main() {
-  querySelector("#sample_text_id")
-      ..text = "Click me!"
-      ..onClick.listen(reverseText);
-}
-
-void reverseText(MouseEvent event) {
-  var text = querySelector("#sample_text_id").text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector("#sample_text_id").text = buffer.toString();
+  applicationFactory().run();
 }
