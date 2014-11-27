94c3c7968dd1f483ee14d047340e1203507ff9c7
Matthieu NICOLAS <matthieu.nicolas@telecomnancy.net>
2014-11-27 10:42:37 +0100

    Implement https://angulardart.org/tutorial/05-ch03-component.html

--- public/dart/web/play-with-dart.dart
+++ public/dart/web/play-with-dart.dart
@@ -1,10 +1,14 @@
 import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
+
+import 'package:play_with_dart/component/rating.dart';
 import 'package:play_with_dart/component/recipe_book.dart';
+
 
 class MyAppModule extends Module {
   MyAppModule() {
     bind(RecipeBookComponent);
+    bind(RatingComponent);
   }
 }
 
